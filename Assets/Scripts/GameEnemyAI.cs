22822f36e736e751c3c561df43e5a59acd99b430
Nadia Morris <reversegremlin@gmail.com>
2019-06-26 16:18:17 -0400

    added gun asset and reticle

--- Assets/Scripts/GameEnemyAI.cs
+++ Assets/Scripts/GameEnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,18 +8,54 @@ public class GameEnemyAI : MonoBehaviour
 {
 
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5.0f;
+
+    bool isProvoked = false;
+
     NavMeshAgent navMeshAgent;
     
-    // Start is called before the first frame update
+    float distanceToTarget = Mathf.Infinity;
+
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (isProvoked) {
+            EngageTarget();
+        } else if (distanceToTarget <= chaseRange) {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance) {
+            ChaseTarget();
+        }
+
+        if  (distanceToTarget <= navMeshAgent.stoppingDistance) {
+            AttackTarget();
+        }
+    }
+
+    private void AttackTarget()
+    {
+        print("EXTERMINATE!!!");
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
 
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDrawGizmosSelected()
     {
-        navMeshAgent.SetDestination(target.position);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
