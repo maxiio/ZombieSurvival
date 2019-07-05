c0a35eadf4bc301bef549c699dae5d141e65a818
Nadia Morris <reversegremlin@gmail.com>
2019-07-05 10:47:13 -0400

    weapon zoom implemented

---
+++ Assets/Scripts/WeaponZoomer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoomer : MonoBehaviour
+{
+
+    [SerializeField] float zoomIn = 20;
+    [SerializeField] float zoomOut = 60;
+
+    [SerializeField] Camera fpsCamera;
+
+
+    bool ZoomedInSwitch = false;
+
+    void SetWeaponZoom(float zoom) {
+        fpsCamera.fieldOfView = zoom;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1)) 
+        {
+            if (ZoomedInSwitch == false) 
+            {
+                ZoomedInSwitch = true;
+                SetWeaponZoom(zoomIn);
+            }
+            else 
+            {
+                ZoomedInSwitch = false;
+                SetWeaponZoom(zoomOut);
+            }
+        }
+    }
+}
