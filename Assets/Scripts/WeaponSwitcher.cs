aac25dc91d4e235fc9194fb1957d74f3d0e918db
Nadia Morris <reversegremlin@gmail.com>
2019-07-06 17:47:39 -0400

    weapon switcher working, tweak weapon placement

---
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+
+    [SerializeField] int currentWeapon = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetActiveWeapon();
+    }
+
+    private void SetActiveWeapon()
+    {
+        int weaponIndex = 0;
+
+        foreach (Transform weapon in transform)
+        {
+            if (weaponIndex == currentWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+            } 
+            else 
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            weaponIndex++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
