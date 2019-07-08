b3f971958b1cd44a8e681b4a5d21e54aaff9c2cd
Nadia Morris <reversegremlin@gmail.com>
2019-07-08 01:16:29 -0400

    hiding gun while zooming, barrel was in view

--- Assets/Scripts/WeaponZoomer.cs
+++ Assets/Scripts/WeaponZoomer.cs
@@ -12,6 +12,7 @@ public class WeaponZoomer : MonoBehaviour
     [SerializeField] float zoomInSensitivity = .5f;
 
     [SerializeField] Camera fpsCamera;
+    [SerializeField] Weapon currentWeapon;
 
     [SerializeField] RigidbodyFirstPersonController fpsController;
     
@@ -27,14 +28,23 @@ public class WeaponZoomer : MonoBehaviour
             if (ZoomedInSwitch == false) 
             {
                 ZoomedInSwitch = true;
+                // currentWeapon.GetComponent<Renderer>().enabled = false;
+                foreach (Transform child in currentWeapon.transform) {
+                    child.GetComponent<Renderer>().enabled = false;
+                }
                 fpsCamera.fieldOfView = zoomInFOV;
                 fpsController.mouseLook.XSensitivity = zoomInSensitivity;
                 fpsController.mouseLook.YSensitivity = zoomInSensitivity;
-
             }
             else 
             {
                 ZoomedInSwitch = false;
+                
+                // currentWeapon.GetComponent<Renderer>().enabled = true;
+                foreach (Transform child in currentWeapon.transform) {
+                    child.GetComponent<Renderer>().enabled = true;
+                }
+
                 fpsCamera.fieldOfView = zoomOutFOV;
                 fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
                 fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
