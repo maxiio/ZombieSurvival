a732ddef576d40a7ce8473d7ac1b3bba6b7872de
Nadia Morris <reversegremlin@gmail.com>
2019-07-22 12:14:53 -0400

    pause working, pulls up menu, currently only stopping time, need to disable weapon/movement

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour 
+{
+    [SerializeField] private GameObject pausePanel;
+    void Start()
+    {
+        pausePanel.SetActive(false);
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown (KeyCode.Escape)) 
+        {
+            Debug.Log("I have pressed escape");
+            if (!pausePanel.activeInHierarchy) 
+            {
+                PauseGame();
+            } else if (pausePanel.activeInHierarchy) 
+            {
+                 ContinueGame();   
+            }
+        } 
+     }
+    private void PauseGame()
+    {
+        Debug.Log("attempting to pause game.");
+        pausePanel.SetActive(true);
+        Time.timeScale = 0;
+        //Disable scripts that still work while timescale is set to 0
+    } 
+    private void ContinueGame()
+    {
+        Debug.Log("attempting to continue game.");
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+        //enable the scripts again
+    }
+}
