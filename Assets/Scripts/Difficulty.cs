55ccb3f372ff562893baab4fb902a037fbca1e10
Nadia Morris <reversegremlin@gmail.com>
2019-07-23 06:10:10 -0400

    merge conflict

--- Assets/Scripts/Difficulty.cs
+++ Assets/Scripts/Difficulty.cs
@@ -12,20 +12,9 @@ using UnityEngine.UI;
 
 public class Difficulty : MonoBehaviour
 {
-    //Make sure to attach these Buttons in the Inspector
     public Button m_YourFirstButton, m_YourSecondButton, m_YourThirdButton, saveButton;
 
-<<<<<<< HEAD:Assets/Scripts/Difficulty.cs
-=======
-    enum DifficultyLevel
-    {
-        Easy = 0,
-        Medium = 1,
-        Hard = 2
-    }
-
     DifficultyLevel difficulty;
->>>>>>> ae3e238c8dc704d4889741d0003ad493202f101a:Assets/Difficulty.cs
 
     void Start()
     {
@@ -50,17 +39,15 @@ public class Difficulty : MonoBehaviour
     void SetDifficulty(DifficultyLevel level) 
     {
         Debug.Log(level);
-<<<<<<< HEAD:Assets/Scripts/Difficulty.cs
         EnemyHealth[] enemies = FindObjectsOfType<EnemyHealth>();
         foreach (EnemyHealth enemy in enemies)
         {
             enemy.SetDifficulty(level);
         }
-=======
+
         int l = (int)level;
         PlayerPrefs.SetInt("difficulty", l);
         Debug.Log((DifficultyLevel)PlayerPrefs.GetInt("difficulty", 1));
->>>>>>> ae3e238c8dc704d4889741d0003ad493202f101a:Assets/Difficulty.cs
     }
 
 
