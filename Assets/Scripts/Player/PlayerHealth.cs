3d9e8f6cc1ac6f62029c337f5055f0b6613e3658
Nadia Morris <reversegremlin@gmail.com>
2019-08-03 21:03:43 -0400

    Player Health HUD

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
 
     [SerializeField] float health = 100f;
     DeathHandler playerDeath;
+    public Slider HealthBar;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +28,7 @@ public class PlayerHealth : MonoBehaviour
         Debug.Log("TakeDamage called with " + damageAmmount + " damage");
         health -= damageAmmount;
         Debug.Log("player health now: " + health);
+        HealthBar.value = health;
 
         if (health <= 0)
         {
