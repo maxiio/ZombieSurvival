3274912e97edb032bdb0c0054aeb1cfe9c368845
Nadia Morris <reversegremlin@gmail.com>
2019-08-23 09:00:06 -0400

    Summary Screen

---
+++ Assets/Scripts/Player/ZombieCounter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieCounter : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    GameObject[] zombies;
+    int totalEnemies = 0;
+    int remainingEnemies = 0;
+
+    void Start()
+    {
+        zombies = GameObject.FindGameObjectsWithTag("Enemy");
+        totalEnemies = zombies.Length;
+        remainingEnemies = totalEnemies;
+    }
+
+    public int GetTotalEnemies()
+    {
+        return totalEnemies;
+    }
+
+    public int GetRemainingEnemies()
+    {
+        return remainingEnemies;
+    }
+
+    // Update is called once per frame
+    public void DecrementEnemies()
+    {
+        remainingEnemies--;
+        Debug.Log("Enemies Remaining: " + remainingEnemies);
+    }
+}
---
+++ Assets/Scripts/UI/ZombieCountUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ZombieCountUI : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI zombieCountText;
+    [SerializeField] ZombieCounter zombieCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        zombieCountText.text = zombieCounter.GetRemainingEnemies() + " / " + zombieCounter.GetTotalEnemies();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
